c9fec9669421597a11e5f056ded59f0265548582
Your Name <amax@redsymbol.net>
2011-03-16 15:53:55 -0700

    Code docs for filters.resizeiframe

--- mobilize/filters.py
+++ mobilize/filters.py
@@ -112,6 +112,12 @@ def resizeobject(elem, width=280):
     several object elements within, those beyond the first will be
     ignored.  Best thing is probably to just find and operate on all
     of them.
+
+    TODO: If the original element defines a height attribute, rather
+    than elimitating it, better to read it, can calculate a new
+    proportional value, so that the height/width ratio is the same
+    before and after the transformation.
+    
     '''
     object_elem = findonetag(elem, 'object')
     if object_elem is not None:
@@ -121,6 +127,21 @@ def resizeobject(elem, width=280):
             _setwidth(embed_elem, width)
 
 def resizeiframe(elem, width=280):
+    '''
+    Resize an iframe to have a mobile-friendly width
+
+    If elem contains (or is) an iframe element, set its width to a
+    mobile-friendly value.  This is handled by deleting the height
+    attribute, if it is present; and setting or adding a width
+    attribute with the indicated value.  This is done for both the
+    "object" tag, and also any "embed" tag that may be present inside.
+
+    This was originally created for the resizing of iframe-based
+    embedded Youtube videos.
+
+    TODO: see TODOs on resizeobject, which are mostly relevant to this filter
+
+    '''
     iframe_elem = findonetag(elem, 'iframe')
     if iframe_elem is not None:
         _setwidth(iframe_elem, width)
