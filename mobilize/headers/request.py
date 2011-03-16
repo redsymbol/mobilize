1ab47ee525e5e3999006fb1273a91182408ab748
Your Name <amax@redsymbol.net>
2011-03-15 22:49:06 -0700

    Request transformation: referrer

--- mobilize/headers/request.py
+++ mobilize/headers/request.py
@@ -4,8 +4,12 @@ def host(environ, value):
 def cookie(environ, value):
     return value
 
+def referer(environ, value):
+    return value.replace(environ['HTTP_HOST'], environ['MWU_OTHER_DOMAIN'], 1)
+
 request_xforms = {
     'host'   : host,
     'cookie' : cookie,
+    'referer' : referer,
     }
 
