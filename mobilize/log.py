7be04c5d722e5319ec5c4cc89bf1f374308f8b02
Your Name <amax@redsymbol.net>
2011-03-26 19:56:52 -0700

    Mobilize webserver logging facility

---
+++ mobilize/log.py
@@ -0,0 +1,41 @@
+'''
+Mobilize logging facilities
+
+'''
+
+class WsgiLogger(object):
+    '''
+    Class for logging to WSGI web server
+    '''
+
+    def __init__(self, outf):
+        '''
+        ctor
+
+        @param outf : File to write log messages to
+        @type  outf : quacks like a file
+        
+        '''
+        self.outf = outf
+
+    @classmethod
+    def create(Cls, environ):
+        '''
+        Create a logger for a wsgi environment
+
+        This will return a logger that writes to the wsgi.errors file,
+        which normally means that messages will be written in the web
+        server error log for this (virtual)host.
+
+        @param environ : WSGI environment
+        @type  environ : dict
+        
+        '''
+        logger = Cls(environ['wsgi.errors'])
+        return logger
+
+    def log(self, msg):
+        '''
+        Write a message
+        '''
+        self.outf.write(msg)
