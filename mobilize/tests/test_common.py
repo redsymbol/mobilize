63544fbc5ba2cc146beed285d2a9768b6343d1ea
Your Name <amax@redsymbol.net>
2011-03-31 14:59:44 -0700

    Merge common tests

--- mobilize/tests/test_common.py
+++ mobilize/tests/test_common.py
@@ -20,3 +20,15 @@ class TestCommon(unittest.TestCase):
             actual = common.fullsiteurl(td['mobileurl'], td['mobiledomain'], td['fullsitedomain'])
             self.assertSequenceEqual(expected, actual)
             
+    def test_classvalue(self):
+        self.assertSequenceEqual('mwu-elem', common.classvalue())
+        self.assertSequenceEqual('mwu-elem mwu-elem-alpha mwu-elem-beta', common.classvalue('alpha', 'beta'))
+
+    def test_classname(self):
+        self.assertSequenceEqual('mwu-elem', common.classname())
+        self.assertSequenceEqual('mwu-elem-alpha', common.classname('alpha'))
+
+    def test_idname(self):
+        self.assertSequenceEqual('mwu-elem-42', common.idname(42))
+        self.assertSequenceEqual('mwu-elem-0', common.idname(0))
+        self.assertSequenceEqual('mwu-elem-blah', common.idname('blah'))
