eae420d0844156effe483bf8c9392f7c8e53ab9f
Your Name <amax@redsymbol.net>
2011-04-02 12:11:02 -0700

    Code documentation for tools/map2links.py

--- tools/map2links.py
+++ tools/map2links.py
@@ -2,6 +2,7 @@
 import os
 from lxml import html
 def get_args():
+    '''fetch arguments from commmand line'''
     import argparse
     parser = argparse.ArgumentParser(description='Generate an HTML list of links from a map menu.')
     parser.add_argument(
@@ -24,6 +25,22 @@ def get_args():
     return parser.parse_args()
 
 def findmap(html_str):
+    '''
+    Find the map element
+
+    Raise an error if this HTML fragment does not contain a MAP element.
+
+    This is normally meant to be invoked on an HTML fragment that
+    contains exactly one map element.  If the fragment contains
+    several, the first found is returned.
+
+    @param html_str : HTML fragment containing a MAP tag
+    @type  html_str : str
+
+    @return         : map element
+    @rtype          : lxml.html.HtmlElement
+    
+    '''
     elem = html.fromstring(html_str)
     if 'map' == elem.tag:
         map_elem = elem
@@ -32,11 +49,33 @@ def findmap(html_str):
     assert map_elem is not None
     return map_elem
 
-def choicedata(map_elem, imgformat):
+def choicedata(map_elem, imageformat):
+    '''
+    Extract and calculate useful information from a map element
+
+    This collects the raw information used to construct a meaningful
+    list of mobile-friendly links.
+
+    imageformat needs to be a Python format string that accepts
+    exactly one integer field.  Something like "/path/to/foo{}.png".
+    This will be rendered with a zero-based integer index
+    corresponding to the link.  So the first choice would render as
+    "/path/to/foo0.png", then "/path/to/foo1.png", and so on.
+
+    @param map_elem    : Map element
+    @type  map_elem    : lxml.html.HtmlElement
+
+    @param imageformat : a Python format string that can be used to generate a choice/link image path
+    @type  imageformat : str
+
+    @return            : Detailed data on possible link choices
+    @rtype             : list of dict
+    
+    '''
     def choice(area_elem, nn):
         def a(key):
             return area_elem.attrib[key]
-        imgsrc = imgformat.format(nn)
+        imgsrc = imageformat.format(nn)
         imgfile = os.path.basename(imgsrc)
         return {
             'alt'    : a('alt'),
@@ -50,7 +89,21 @@ def choicedata(map_elem, imgformat):
 
 def im_geom(coords):
     '''
-    converts a coordinates string (from the map tag coords attrib) to imagemagick geom string
+    converts area tag coordinates string to imagemagick geometry string
+
+    coords is the value of the "coords" attrib of an AREA tag,
+    corresponding to one possible choice in a MAP element.
+
+    The returned geometry string defines a subset of the map source
+    image, which can be directly passed to the ImageMagick library to
+    extract the clickable/touchable are of the map.
+
+    @param coords : The value of the "coords" attribute of the area tag
+    @type  coords : str
+
+    @return       : The equivalent geometry that will extract the area from the map's source image
+    @rtype        : str
+    
     '''
     parts = map(int, coords.split(','))
     x1, y1, x2, y2 = parts
@@ -59,12 +112,15 @@ def im_geom(coords):
     return '{}x{}+{}+{}'.format(width, height, x1, y1)
 
 def convertcmd(coords, mapimg, outimg):
+    '''
+    Calculate the command-line "convert" command for generating a single choice image
+    '''
     return 'convert -crop {geom} {mapimg} {outimg}'.format(
         geom   = im_geom(coords),
         mapimg = mapimg,
         outimg = outimg,
         )
-    
+
 if '__main__' == __name__:
     import sys
     args = get_args()
@@ -79,7 +135,6 @@ if '__main__' == __name__:
     for choice in choices:
         htmlout += convertcmd(choice['coords'], args.mapimage, choice['imgfile']) + '\n'
     htmlout += '-->\n'
-
     print(htmlout)
     
 
