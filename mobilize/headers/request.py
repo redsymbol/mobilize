b7d59428c98db73a9736e493122731a17315e39a
Aaron Maxwell <amax@redsymbol.net>
2011-06-07 11:23:50 -0700

    omit unused request header xform

--- mobilize/headers/request.py
+++ mobilize/headers/request.py
@@ -1,14 +1,9 @@
 def host(environ, value):
     return environ['MWU_OTHER_DOMAIN']
 
-def cookie(environ, value):
-    return value
-
 def source_referer(environ, value):
     return value.replace(environ['HTTP_HOST'], environ['MWU_OTHER_DOMAIN'], 1)
 
 request_xforms = {
     'host'   : host,
-    'cookie' : cookie,
     }
-
