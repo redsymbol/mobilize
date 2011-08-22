3e8eece78973122bf031a28cbe8aaa77e9b952f8
Aaron Maxwell <amax@redsymbol.net>
2011-08-22 16:11:43 -0700

    Set-Cookie: set on root domain

--- mobilize/headers/response.py
+++ mobilize/headers/response.py
@@ -2,8 +2,17 @@
 def location(environ, value):
     return value.replace(environ['MWU_OTHER_DOMAIN'], environ['HTTP_HOST'], 1)
 
+import re
+_set_cookie_re = re.compile(r'(?P<prefix>\bdomain\s*=\s*)(?P<domain>[^ ;]+)', re.I)
+def _set_cookie_repl(match):
+    domain = match.group('domain')
+    pos = domain.find('.')
+    if pos > 1 and (domain.count('.') == 2):
+        domain = domain[pos:]
+    return match.group('prefix') + domain
+
 def set_cookie(environ, value):
-    return value
+    return _set_cookie_re.sub(_set_cookie_repl, value, count=1)
 
 response_xforms = {
     'location'   : location,
---
+++ mobilize/tests/test_headers_response.py
@@ -0,0 +1,27 @@
+import unittest
+
+class TestResponse(unittest.TestCase):
+    def test_set_cookie(self):
+        testdata = [
+            {'in' : 'PHPSESSID=0s506lc6m6p7o5m0l2mv5d51a2; Path=/',
+             'out' : 'PHPSESSID=0s506lc6m6p7o5m0l2mv5d51a2; Path=/',
+             },
+            {'in' : 'PHPSESSID=0s506lc6m6p7o5m0l2mv5d51a2; Domain=secure.msia.org; Path=/',
+             'out' : 'PHPSESSID=0s506lc6m6p7o5m0l2mv5d51a2; Domain=.msia.org; Path=/',
+             },
+            {'in' : 'PHPSESSID=0s506lc6m6p7o5m0l2mv5d51a2; Domain=.msia.org; Path=/',
+             'out' : 'PHPSESSID=0s506lc6m6p7o5m0l2mv5d51a2; Domain=.msia.org; Path=/',
+             },
+            {'in' : 'zed=1; Domain=foo.example.org',
+             'out' : 'zed=1; Domain=.example.org',
+             },
+            {'in' : 'zed=1; Domain=example.org',
+             'out' : 'zed=1; Domain=example.org',
+             },
+            ]
+        from mobilize.headers.response import set_cookie
+        environ = {}
+        for ii, td in enumerate(testdata):
+            expected = td['out']
+            actual = set_cookie(environ, td['in'])
+            self.assertSequenceEqual(expected, actual)
