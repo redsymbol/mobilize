5b5bc12293debcf870e937cd014f5ef6e31ee669
Mobile Web Up <amax@mobilewebup.com>
2012-06-01 11:06:24 -0700

    Remove old and probably outdated comment
    It's documenting a heisenbug that I haven't encountered in a very long while.
    Seems more likely at this point that it was fixed by some other changes.

--- mobilize/httputil.py
+++ mobilize/httputil.py
@@ -482,7 +482,6 @@ def mk_wsgi_application(msite):
             if handler == passthrough:
                 # Nothing to do here...
                 raise
-            # TODO: Sometimes this will block, under conditions I haven't characterized yet.  Maybe when start_response is already invoked?
             return response(passthrough)
     return application
 
